78088ebb9903f070aacb3f1cd0cb09d3da183383
jalindardhawalestw-services <167845100+jalindardhawalestw-services@users.noreply.github.com>
2024-04-23 16:30:59 +0530

    Update VannaAI.py

--- VannaAI.py
+++ VannaAI.py
@@ -3,15 +3,15 @@ from vanna.remote import VannaDefault
 from vanna.flask import VannaFlaskApp
 
 # Set your API key and model name
-api_key = '65972c34807d425b9ede327daabfc981'
-vanna_model_name = 'stwai'
+
 
 # Initialize VannaDefault with your API key and model name
+api_key='65972c34807d425b9ede327daabfc981'
+vanna_model_name='stw_ai'
 vn = VannaDefault(model=vanna_model_name, api_key=api_key)
-
 # Connect to your MSSQL database using the appropriate ODBC connection string
-odbc_conn_str = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=STWDOTNETSERVER\\DEV19;DATABASE=ADAM;UID=stw;PWD=StW@2021*!$'
-vn.connect_to_mssql(odbc_conn_str=odbc_conn_str)
+vn.connect_to_mssql(odbc_conn_str='DRIVER={ODBC Driver 17 for SQL Server};SERVER=Demo\\SQLDEV2022;DATABASE=nop_commerce;UID=sa;PWD=12345')
+# vn.connect_to_mssql(odbc_conn_str=odbc_conn_str)
 
 # Create a VannaFlaskApp instance with your VannaDefault object
 app = VannaFlaskApp(vn)
